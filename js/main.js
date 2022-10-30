44586ef0988b137260ca973a6a4b6c580d1af461
Vladimir Kabin <kabinvlad@gmail.com>
2022-10-30 22:53:58 +0100

    Массива из 25 сгенерированных объектов

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-function getRandomPositivInteger(a, b) {
+function getRandomPositiveInteger (a, b) {
   if (a < 0 || b < 0) {
-    return;
+    return NaN;
   }
   const lower = Math.ceil(Math.min(a, b));
   const upper = Math.floor(Math.max(a, b));
@@ -8,9 +8,25 @@ function getRandomPositivInteger(a, b) {
   return Math.floor(result);
 }
 
-function checkStringLength(string, length) {
-  return string.length < length;
+function checkStringLength (string, length) {
+  return string.length <= length;
 }
 
-getRandomPositivInteger(1, 0);
-checkStringLength('', 140);
+let indexID = 0;
+
+const createID = function() {
+  indexID++;
+  return indexID;
+}
+
+function createPhoto() {
+  return {
+    id: createID(),
+    url: 'photos/${indexID}.jpg',
+    description: 'Описание фото',
+    likes: getRandomPositiveInteger(15, 200),
+    comments: getRandomPositiveInteger(0, 200),
+  }
+}
+
+const resultArray = Array.from({length: 25}, createPhoto);
